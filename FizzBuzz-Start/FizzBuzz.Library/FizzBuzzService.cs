e47598f4ccef219a306688a5da02ebebbdb7f3ec
Fernando Feliu Goncalez <fernando.goncalez@autotrac.com.br>
2021-12-28 16:44:39 -0300

    Pequeno exemplo aplicando TDD para os alunos de Metricas/Qualidade de Software

--- FizzBuzz-Start/FizzBuzz.Library/FizzBuzzService.cs
+++ FizzBuzz-Start/FizzBuzz.Library/FizzBuzzService.cs
@@ -1,4 +1,36 @@
 namespace FizzBuzz.Library
 {
+    public class FizzBuzzService
+    {
+        public string Print(int n)
+        {
+            if(IsDisivibleForFive(n) && IsDisivibleForThree(n))
+            {
+                return "FizzBuzz";
+            }
+            else if(IsDisivibleForFive(n))
+            {
+                return "Buzz";
+            }
+            else if(IsDisivibleForThree(n))
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return n.ToString();
+            }
+            
+        }
 
+        private bool IsDisivibleForThree(int n)
+        {
+            return n % 3 == 0;
+        }
+
+        private bool IsDisivibleForFive(int n)
+        {
+            return n % 5 == 0;
+        }        
+    }
 }
--- FizzBuzz-Start/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz-Start/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -6,6 +6,36 @@ namespace FizzBuzz.Tests
     [TestClass]
     public class FizzBuzzTests
     {
+        private FizzBuzzService _fizzBuzzService;
 
+        public FizzBuzzTests()
+        {
+            _fizzBuzzService = new FizzBuzzService();
+        }
+
+        [TestMethod]
+        public void ShouldPrintNumber()
+        {
+            Assert.AreEqual("1", _fizzBuzzService.Print(1));
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizz()
+        {
+            Assert.AreEqual("Fizz", _fizzBuzzService.Print(3));
+        }        
+
+        [TestMethod]
+        public void ShouldPrintBuzz()
+        {
+            Assert.AreEqual("Buzz", _fizzBuzzService.Print(5));
+        }    
+
+        [TestMethod]
+        public void ShouldPrintFizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", _fizzBuzzService.Print(15));
+            Assert.AreEqual("FizzBuzz", _fizzBuzzService.Print(30));
+        }               
     }
 }
